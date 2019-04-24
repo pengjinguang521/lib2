d638ad38caecf61fa555aa562b7893f60d1acec6
彭金光 <745758483@qq.com>
2019-04-24 13:56:51 +0800

    第一次提交代码

---
+++ lib2/lib2.h
@@ -0,0 +1,15 @@
+//
+//  lib2.h
+//  lib2
+//
+//  Created by iosgroup on 2019/4/24.
+//  Copyright © 2019 iosgroup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface lib2 : NSObject
+
+- (void)testLib2;
+
+@end
---
+++ lib2/lib2.m
@@ -0,0 +1,17 @@
+//
+//  lib2.m
+//  lib2
+//
+//  Created by iosgroup on 2019/4/24.
+//  Copyright © 2019 iosgroup. All rights reserved.
+//
+
+#import "lib2.h"
+
+@implementation lib2
+
+- (void)testLib2 {
+    NSLog(@"testLib2");
+}
+
+@end
